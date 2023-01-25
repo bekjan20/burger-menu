281498a563bdb6bfe110d1863f4ba561e93ef0e7
bekjan20 <bekzanabdilazizov38@gmail.com>
2023-01-25 12:50:39 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+let burger  = document.querySelector('#icon')
+let list = document.querySelector('.list')
+burger.addEventListener('click', f)
+
+function f (){
+list.classList.toggle('newList')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
